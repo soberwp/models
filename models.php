66e1d933f638cf1bf725b523bad90be5afbab3c0
Darren Jacoby <darren@jacoby.co.za>
2018-08-14 12:12:12 +0200

    Resolve

--- models.php
+++ models.php
@@ -1,20 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-=======
-/*
-Plugin Name:        Models
-Plugin URI:         http://github.com/soberwp/models
-Description:        WordPress plugin to create custom post types and taxonomies using JSON, YAML or PHP files
-Version:            1.1.0
-Author:             Sober
-Author URI:         http://github.com/soberwp/
-License:            MIT License
-License URI:        http://opensource.org/licenses/MIT
-GitHub Plugin URI:  soberwp/models
-GitHub Branch:      master
-*/
->>>>>>> master
 namespace Sober\Models;
 
 use Sober\Models\Loader;
